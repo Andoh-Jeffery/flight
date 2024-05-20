05ea5610c16d062a9dbe75fd2ef6381feab3130b
TroyLynx <58670183+Andoh-Jeffery@users.noreply.github.com>
2024-05-20 07:39:44 +0100

    db migrations 4 aircraft and flights

---
+++ database/migrations/2024_05_20_035737_create_aircraft_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aircraft', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('serial_number');
+            $table->string('manufacturer');
+            $table->string('model');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aircraft');
+    }
+};
---
+++ database/migrations/2024_05_20_062521_create_flights_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('flight_number');
+            $table->foreignId('aircraft_id')->constrained();
+            $table->string('origin');
+            $table->string('destination');
+            $table->date('date');
+            $table->time('dep_time');
+            $table->time('arr_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
